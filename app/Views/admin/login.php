ecef4f14def6cdea3cd6752b1ccaa040affcebb5
nuranoer <nuranoer7@gmail.com>
2022-07-05 12:03:48 +0700

    View Login

--- app/Views/admin/login.php
+++ app/Views/admin/login.php
@@ -20,7 +20,7 @@
 
 	<!-- Document Title
 	============================================= -->
-	<title>Login - Layout 5 | Canvas</title>
+	<title>Login</title>
 
 </head>
 
@@ -35,24 +35,25 @@
 		<section id="content">
 			<div class="content-wrap py-0">
 
-				<div class="section p-0 m-0 h-100 position-absolute" style="background: url('HTML/images/parallax/home/1.jpg') center center no-repeat; background-size: cover;"></div>
+				<div class="section p-0 m-0 h-100 position-absolute" style="background: url('HTML/images/login/imigrasi1.jpg') center center no-repeat; background-size: cover;"></div>
 
 				<div class="section bg-transparent min-vh-100 p-0 m-0">
 					<div class="vertical-middle">
 						<div class="container-fluid py-5 mx-auto">
-							<div class="center">
-								<a href="/loginadmin"><img src="HTML/images/logo-dark.png" alt="Canvas Logo"></a>
-							</div>
+							
 
 							<div class="card mx-auto rounded-0 border-0" style="max-width: 400px; background-color: rgba(255,255,255,0.93);">
 								<div class="card-body" style="padding: 40px;">
 									<form id="login-form" name="login-form" class="mb-0" action="#" method="post">
-										<h3>Login to your Account</h3>
-
+                                    <div class="center">
+								<a href="/loginadmin"><img src="HTML/images/login/logo.png" alt="Imigrasi Logo" width="150"></a>
+                                <h4>Imigrasi Kediri</h4>
+							</div>
+                                        
 										<div class="row">
 											<div class="col-12 form-group">
-												<label for="login-form-username">Username:</label>
-												<input type="text" id="login-form-username" name="login-form-username" value="" class="form-control not-dark" />
+												<label for="login-form-nip">NIP:</label>
+												<input type="text" id="login-form-nip" name="login-form-nip" value="" class="form-control not-dark" />
 											</div>
 
 											<div class="col-12 form-group">
@@ -60,9 +61,10 @@
 												<input type="password" id="login-form-password" name="login-form-password" value="" class="form-control not-dark" />
 											</div>
 
-											<div class="col-12 form-group">
-												<button class="button button-3d button-black m-0" id="login-form-submit" name="login-form-submit" value="login">Login</button>
-												<a href="#" class="float-end">Forgot Password?</a>
+											<div class="col-12 form-group justify-content-center text-center">
+                                                <a href="£" class="button button-3d button-black m-0" id="login-form-submit" name="login-form-submit" value="login">Login</a>
+												<!-- <button class="button button-3d button-black m-0 position-center" id="login-form-submit" name="login-form-submit" value="login">Login</button> -->
+												
 											</div>
 										</div>
 									</form>
@@ -70,7 +72,7 @@
 								</div>
 							</div>
 
-							<div class="text-center dark mt-3"><small>Copyrights &copy; All Rights Reserved by Canvas Inc.</small></div>
+							<div class="text-center dark mt-3"><small>Copyrights &copy; Kantor Imigrasi Kelas II Non TPI Kediri</small></div>
 						</div>
 					</div>
 				</div>
