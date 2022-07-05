ff14bd67e04c23adcc70122682789dc3dc23dd9d
tataadn <adine.pramesti@gmail.com>
2022-07-05 19:01:46 +0700

    layout view all pages

---
+++ app/Views/layout/jumbotron.php
@@ -0,0 +1,46 @@
+        <section id="slider" class="slider-element slider-parallax swiper_wrapper min-vh-60 min-vh-md-100 include-header">
+			<div class="slider-inner">
+				<div class="swiper-container swiper-parent">
+					<div class="swiper-wrapper">
+						<div class="swiper-slide dark">
+							<div class="container">
+								<div class="slider-caption slider-caption-center">
+									<h2 data-animate="fadeInUp">Welcome to Website Si Cepot</h2>
+									<p class="d-none d-sm-block" data-animate="fadeInUp" data-delay="200">Kantor Imigrasi Kelas II Non TPI Kediri <br>Jl. Jawa No.135, Bedrek Selatan, Grogol, Kec. Grogol, Kabupaten Kediri, Jawa Timur 64151</p>
+								</div>
+							</div>
+							<div class="swiper-slide-bg" style="background-image: url('HTML/images/slider/swiper/imigrasi1.jpg');"></div>
+						</div>
+						<div class="swiper-slide dark">
+							<div class="container">
+								<div class="slider-caption slider-caption-center">
+									<h2 data-animate="fadeInUp">Beautifully Flexible</h2>
+									<p class="d-none d-sm-block" data-animate="fadeInUp" data-delay="200">Looks beautiful &amp; ultra-sharp on Retina Screen Displays. Powerful Layout with Responsive functionality that can be adapted to any screen size.</p>
+								</div>
+							</div>
+							<div class="video-wrap">
+								<video id="slide-video" poster="HTML/images/videos/explore-poster.jpg" preload="auto" loop autoplay muted>
+									<source src='HTML/images/videos/explore.webm' type='video/webm' />
+									<source src='HTML/images/videos/explore.mp4' type='video/mp4' />
+								</video>
+								<div class="video-overlay" style="background-color: rgba(0,0,0,0.55);"></div>
+							</div>
+						</div>
+						<div class="swiper-slide">
+							<div class="container">
+								<div class="slider-caption">
+									<h2 data-animate="fadeInUp">Great Performance</h2>
+									<p class="d-none d-sm-block" data-animate="fadeInUp" data-delay="200">You'll be surprised to see the Final Results of your Creation &amp; would crave for more.</p>
+								</div>
+							</div>
+							<div class="swiper-slide-bg" style="background-image: url('HTML/images/slider/swiper/3.jpg'); background-position: center top;"></div>
+						</div>
+					</div>
+					<div class="slider-arrow-left"><i class="icon-angle-left"></i></div>
+					<div class="slider-arrow-right"><i class="icon-angle-right"></i></div>
+				</div>
+
+				<a href="#" data-scrollto="#content" data-offset="100" class="one-page-arrow dark"><i class="icon-angle-down infinite animated fadeInDown"></i></a>
+
+			</div>
+		</section>
