9a88d18e7022f00c245c07b1c96939b9adf3ba2d
tataadn <adine.pramesti@gmail.com>
2022-07-05 20:27:02 +0700

    login

--- app/Views/admin/login.php
+++ app/Views/admin/login.php
@@ -5,6 +5,8 @@
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<meta name="author" content="SemiColonWeb" />
 
+	<link rel="icon" href="HTML/images/pemohon/logoimigrasi.png" type="image/gif">
+
 	<!-- Stylesheets
 	============================================= -->
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Poppins:300,400,500,600,700|PT+Serif:400,400i&display=swap" rel="stylesheet" type="text/css" />
@@ -34,49 +36,43 @@
 		============================================= -->
 		<section id="content">
 			<div class="content-wrap py-0">
-
 				<div class="section p-0 m-0 h-100 position-absolute" style="background: url('HTML/images/login/imigrasi1.jpg') center center no-repeat; background-size: cover;"></div>
+					<div class="section bg-transparent min-vh-100 p-0 m-0">
+						<div class="vertical-middle">
+							<div class="container-fluid py-5 mx-auto">
+								<div class="card mx-auto rounded-0 border-0" style="max-width: 400px; background-color: rgba(255,255,255,0.93);">
+									<div class="card-body" style="padding: 28px;">
+										<form id="login-form" name="login-form" class="mb-0" action="#" method="post">
+											<div class="center">
+												<a href="/loginadmin"><img src="HTML/images/login/logo.png" alt="Imigrasi Logo" width="140"></a>
+												<h4>Imigrasi Kediri</h4>
+											</div>        
+											<div class="row">
+												<div class="col-12 form-group">
+													<label for="login-form-nip">NIP:</label>
+													<input type="text" id="login-form-nip" name="login-form-nip" value="" class="form-control not-dark" />
+												</div>
 
-				<div class="section bg-transparent min-vh-100 p-0 m-0">
-					<div class="vertical-middle">
-						<div class="container-fluid py-5 mx-auto">
-							
+												<div class="col-12 form-group">
+													<label for="login-form-password">Password:</label>
+													<input type="password" id="login-form-password" name="login-form-password" value="" class="form-control not-dark" />
+												</div>
 
-							<div class="card mx-auto rounded-0 border-0" style="max-width: 400px; background-color: rgba(255,255,255,0.93);">
-								<div class="card-body" style="padding: 40px;">
-									<form id="login-form" name="login-form" class="mb-0" action="#" method="post">
-                                    <div class="center">
-								<a href="/loginadmin"><img src="HTML/images/login/logo.png" alt="Imigrasi Logo" width="150"></a>
-                                <h4>Imigrasi Kediri</h4>
-							</div>
-                                        
-										<div class="row">
-											<div class="col-12 form-group">
-												<label for="login-form-nip">NIP:</label>
-												<input type="text" id="login-form-nip" name="login-form-nip" value="" class="form-control not-dark" />
+												<div class="col-12 form-group justify-content-center text-center">
+													<a href="/dashboardadmin" class="button button-3d button-black m-0" id="login-form-submit" name="login-form-submit" value="login">Login</a>
+													<!-- <button class="button button-3d button-black m-0 position-center" id="login-form-submit" name="login-form-submit" value="login">Login</button> -->	
+												</div>
 											</div>
-
-											<div class="col-12 form-group">
-												<label for="login-form-password">Password:</label>
-												<input type="password" id="login-form-password" name="login-form-password" value="" class="form-control not-dark" />
-											</div>
-
-											<div class="col-12 form-group justify-content-center text-center">
-                                                <a href="/dashboardadmin" class="button button-3d button-black m-0" id="login-form-submit" name="login-form-submit" value="login">Login</a>
-												<!-- <button class="button button-3d button-black m-0 position-center" id="login-form-submit" name="login-form-submit" value="login">Login</button> -->
-												
-											</div>
-										</div>
-									</form>
-
+										</form>
+									</div>
+								</div>
+								<div class="text-center dark mt-3">
+									<small>Copyrights &copy; Kantor Imigrasi Kelas II Non TPI Kediri</small>
 								</div>
 							</div>
-
-							<div class="text-center dark mt-3"><small>Copyrights &copy; Kantor Imigrasi Kelas II Non TPI Kediri</small></div>
 						</div>
 					</div>
 				</div>
-
 			</div>
 		</section><!-- #content end -->
 
