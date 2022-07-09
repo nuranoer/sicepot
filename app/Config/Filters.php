3fc3b5344e5054f5b99e6f32c4238b79418e4afd
tataadn <adine.pramesti@gmail.com>
2022-07-09 19:29:29 +0700

    filter login page

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -73,6 +73,6 @@ class Filters extends BaseConfig
      * @var array
      */
     public $filters = [
-        'login' => ['before' => ['dashboardadmin']],
+        'login' => ['before' => ['dashboardadmin','perdimdewasa','perdimanak','dataperdimdewasa','dataperdimanak']],
     ];
 }
