37154379062dafd77eca758cf5aef58381a82015
tataadn <adine.pramesti@gmail.com>
2022-07-14 10:57:18 +0700

    validation perdim anak

--- app/Controllers/PemohonAnak.php
+++ app/Controllers/PemohonAnak.php
@@ -13,93 +13,118 @@ class PemohonAnak extends BaseController
     }
     public function perdimanak()
     {
-        return view('pemohon/perdimanak');
+        $data = [
+            'validation' => \Config\Services::validation()
+        ];
+        return view('pemohon/perdimanak', $data);
     }
     public function createperdimanak()
     {
-        // dd($this->request->getVar('address_ortu'));
-
-        if ($this->request->getVar('address_ortu')=='Ya')
-        {
-            $this->PerdimAnakModel->save([
-                'jenis_permohonan' => $this->request->getVar('jenis_permohonan'),
-                'nama_anak' => $this->request->getVar('nama_anak'),
-                'jenis_kelamin' => $this->request->getVar('jenis_kelamin'),
-                'ttl_anak' => $this->request->getVar('ttl_anak'),
-                'no_paspor_anak' => $this->request->getVar('no_paspor_anak'),
-                'no_reg_anak' => $this->request->getVar('no_reg_anak'),
-                'nik' => $this->request->getVar('nik'),
-                'alamat_anak' => $this->request->getVar('alamat_anak'),
-                'tgl_berlaku_kia' => $this->request->getVar('tgl_berlaku_kia'),
-                'rentang_tgl_kia' => $this->request->getVar('rentang_tgl_kia'),
-                'no_hp' => $this->request->getVar('no_hp'),
-                'nama_ibu' => $this->request->getVar('nama_ibu'),
-                'alamat_ibu' => $this->request->getVar('alamat_anak'),
-                'alamat_ayah' => $this->request->getVar('alamat_anak'),
-                'ttl_ibu' => $this->request->getVar('ttl_ibu'),
-                'no_ktp_ibu' => $this->request->getVar('no_ktp_ibu'),
-                'tgl_ktp_ibu' => $this->request->getVar('tgl_ktp_ibu'),
-                'rentang_ktp_ibu' => $this->request->getVar('rentang_ktp_ibu'),
-                'no_paspor_ibu' => $this->request->getVar('no_paspor_ibu'),
-                'tgl_paspor_ibu' => $this->request->getVar('tgl_paspor_ibu'),
-                'rentang_paspor_ibu' => $this->request->getVar('rentang_paspor_ibu'),
-                'nama_ayah' => $this->request->getVar('nama_ayah'),
-                'ttl_ayah' => $this->request->getVar('ttl_ayah'),
-                'no_ktp_ayah' => $this->request->getVar('no_ktp_ayah'),
-                'tgl_ktp_ayah' => $this->request->getVar('tgl_ktp_ayah'),
-                'rentang_ktp_ayah' => $this->request->getVar('rentang_ktp_ayah'),
-                'no_paspor_ayah' => $this->request->getVar('no_paspor_ayah'),
-                'tgl_paspor_ayah' => $this->request->getVar('tgl_paspor_ayah'),
-                'rentang_paspor_ayah' => $this->request->getVar('rentang_paspor_ayah'),
-                'nama_kakek' => $this->request->getVar('nama_kakek'),
-                'pekerjaan' => $this->request->getVar('pekerjaan'),
-                'status_sipil' => $this->request->getVar('status_sipil'),
-                'negara' => $this->request->getVar('negara'),
-                'tujuan' => $this->request->getVar('tujuan'),
-            ]);
+        //validate input
+        if(!$this->validate([
+            'jenis_permohonan' => 'required',
+            'nama_anak' => 'required',
+            'jenis_kelamin' => 'required',
+            'nik' => 'required|is_unique[perdim_anak.nik]|max_length[16]', 
+            'ttl_anak' => 'required',
+            'nik' => 'required',
+            'alamat_anak' => 'required',
+            'no_hp' => 'required',
+            'tujuan' => 'required',
+            'negara' => 'required',
+            'pekerjaan' => 'required',
+        ])) {
+            // $validation = \Config\Services::validation();
+            session()->setFlashdata('error','Mohon cek kembali data Anda!');
+            return redirect()->to('/perdimanak')->withInput();
         } 
         
-        elseif ($this->request->getVar('address_ortu')=='Tidak') 
-        {
-            $this->PerdimAnakModel->save([
-                'jenis_permohonan' => $this->request->getVar('jenis_permohonan'),
-                'nama_anak' => $this->request->getVar('nama_anak'),
-                'jenis_kelamin' => $this->request->getVar('jenis_kelamin'),
-                'ttl_anak' => $this->request->getVar('ttl_anak'),
-                'no_paspor_anak' => $this->request->getVar('no_paspor_anak'),
-                'no_reg_anak' => $this->request->getVar('no_reg_anak'),
-                'nik' => $this->request->getVar('nik'),
-                'alamat_anak' => $this->request->getVar('alamat_anak'),
-                'tgl_berlaku_kia' => $this->request->getVar('tgl_berlaku_kia'),
-                'rentang_tgl_kia' => $this->request->getVar('rentang_tgl_kia'),
-                'no_hp' => $this->request->getVar('no_hp'),
-                'nama_ibu' => $this->request->getVar('nama_ibu'),
-                'alamat_ibu' => $this->request->getVar('alamat_ibu'),
-                'alamat_ayah' => $this->request->getVar('alamat_ayah'),
-                'ttl_ibu' => $this->request->getVar('ttl_ibu'),
-                'no_ktp_ibu' => $this->request->getVar('no_ktp_ibu'),
-                'tgl_ktp_ibu' => $this->request->getVar('tgl_ktp_ibu'),
-                'rentang_ktp_ibu' => $this->request->getVar('rentang_ktp_ibu'),
-                'no_paspor_ibu' => $this->request->getVar('no_paspor_ibu'),
-                'tgl_paspor_ibu' => $this->request->getVar('tgl_paspor_ibu'),
-                'rentang_paspor_ibu' => $this->request->getVar('rentang_paspor_ibu'),
-                'nama_ayah' => $this->request->getVar('nama_ayah'),
-                'ttl_ayah' => $this->request->getVar('ttl_ayah'),
-                'no_ktp_ayah' => $this->request->getVar('no_ktp_ayah'),
-                'tgl_ktp_ayah' => $this->request->getVar('tgl_ktp_ayah'),
-                'rentang_ktp_ayah' => $this->request->getVar('rentang_ktp_ayah'),
-                'no_paspor_ayah' => $this->request->getVar('no_paspor_ayah'),
-                'tgl_paspor_ayah' => $this->request->getVar('tgl_paspor_ayah'),
-                'rentang_paspor_ayah' => $this->request->getVar('rentang_paspor_ayah'),
-                'nama_kakek' => $this->request->getVar('nama_kakek'),
-                'pekerjaan' => $this->request->getVar('pekerjaan'),
-                'status_sipil' => $this->request->getVar('status_sipil'),
-                'negara' => $this->request->getVar('negara'),
-                'tujuan' => $this->request->getVar('tujuan'),
-            ]);
-        };
-
-        session()->setFlashdata('success', 'Data berhasil ditambahkan!');
-        return redirect()->to('/perdimanak');
+        else {
+            
+            if ($this->request->getVar('address_ortu')=='Ya')
+            {
+                $this->PerdimAnakModel->save([
+                    'jenis_permohonan' => $this->request->getVar('jenis_permohonan'),
+                    'nama_anak' => $this->request->getVar('nama_anak'),
+                    'jenis_kelamin' => $this->request->getVar('jenis_kelamin'),
+                    'ttl_anak' => $this->request->getVar('ttl_anak'),
+                    'alasan_penggantian' => $this->request->getVar('alasan_penggantian'),
+                    'no_paspor_anak' => $this->request->getVar('no_paspor_anak'),
+                    'no_reg_anak' => $this->request->getVar('no_reg_anak'),
+                    'nik' => $this->request->getVar('nik'),
+                    'alamat_anak' => $this->request->getVar('alamat_anak'),
+                    'tgl_berlaku_kia' => $this->request->getVar('tgl_berlaku_kia'),
+                    'rentang_tgl_kia' => $this->request->getVar('rentang_tgl_kia'),
+                    'no_hp' => $this->request->getVar('no_hp'),
+                    'nama_ibu' => $this->request->getVar('nama_ibu'),
+                    'alamat_ibu' => $this->request->getVar('alamat_anak'),
+                    'alamat_ayah' => $this->request->getVar('alamat_anak'),
+                    'ttl_ibu' => $this->request->getVar('ttl_ibu'),
+                    'no_ktp_ibu' => $this->request->getVar('no_ktp_ibu'),
+                    'tgl_ktp_ibu' => $this->request->getVar('tgl_ktp_ibu'),
+                    'rentang_ktp_ibu' => $this->request->getVar('rentang_ktp_ibu'),
+                    'no_paspor_ibu' => $this->request->getVar('no_paspor_ibu'),
+                    'tgl_paspor_ibu' => $this->request->getVar('tgl_paspor_ibu'),
+                    'rentang_paspor_ibu' => $this->request->getVar('rentang_paspor_ibu'),
+                    'nama_ayah' => $this->request->getVar('nama_ayah'),
+                    'ttl_ayah' => $this->request->getVar('ttl_ayah'),
+                    'no_ktp_ayah' => $this->request->getVar('no_ktp_ayah'),
+                    'tgl_ktp_ayah' => $this->request->getVar('tgl_ktp_ayah'),
+                    'rentang_ktp_ayah' => $this->request->getVar('rentang_ktp_ayah'),
+                    'no_paspor_ayah' => $this->request->getVar('no_paspor_ayah'),
+                    'tgl_paspor_ayah' => $this->request->getVar('tgl_paspor_ayah'),
+                    'rentang_paspor_ayah' => $this->request->getVar('rentang_paspor_ayah'),
+                    'nama_kakek' => $this->request->getVar('nama_kakek'),
+                    'pekerjaan' => $this->request->getVar('pekerjaan'),
+                    'status_sipil' => $this->request->getVar('status_sipil'),
+                    'negara' => $this->request->getVar('negara'),
+                    'tujuan' => $this->request->getVar('tujuan'),
+                ]);
+            } 
+            
+            elseif ($this->request->getVar('address_ortu')=='Tidak') 
+            {
+                $this->PerdimAnakModel->save([
+                    'jenis_permohonan' => $this->request->getVar('jenis_permohonan'),
+                    'nama_anak' => $this->request->getVar('nama_anak'),
+                    'jenis_kelamin' => $this->request->getVar('jenis_kelamin'),
+                    'ttl_anak' => $this->request->getVar('ttl_anak'),
+                    'alasan_penggantian' => $this->request->getVar('alasan_penggantian'),
+                    'no_paspor_anak' => $this->request->getVar('no_paspor_anak'),
+                    'no_reg_anak' => $this->request->getVar('no_reg_anak'),
+                    'nik' => $this->request->getVar('nik'),
+                    'alamat_anak' => $this->request->getVar('alamat_anak'),
+                    'tgl_berlaku_kia' => $this->request->getVar('tgl_berlaku_kia'),
+                    'rentang_tgl_kia' => $this->request->getVar('rentang_tgl_kia'),
+                    'no_hp' => $this->request->getVar('no_hp'),
+                    'nama_ibu' => $this->request->getVar('nama_ibu'),
+                    'alamat_ibu' => $this->request->getVar('alamat_ibu'),
+                    'alamat_ayah' => $this->request->getVar('alamat_ayah'),
+                    'ttl_ibu' => $this->request->getVar('ttl_ibu'),
+                    'no_ktp_ibu' => $this->request->getVar('no_ktp_ibu'),
+                    'tgl_ktp_ibu' => $this->request->getVar('tgl_ktp_ibu'),
+                    'rentang_ktp_ibu' => $this->request->getVar('rentang_ktp_ibu'),
+                    'no_paspor_ibu' => $this->request->getVar('no_paspor_ibu'),
+                    'tgl_paspor_ibu' => $this->request->getVar('tgl_paspor_ibu'),
+                    'rentang_paspor_ibu' => $this->request->getVar('rentang_paspor_ibu'),
+                    'nama_ayah' => $this->request->getVar('nama_ayah'),
+                    'ttl_ayah' => $this->request->getVar('ttl_ayah'),
+                    'no_ktp_ayah' => $this->request->getVar('no_ktp_ayah'),
+                    'tgl_ktp_ayah' => $this->request->getVar('tgl_ktp_ayah'),
+                    'rentang_ktp_ayah' => $this->request->getVar('rentang_ktp_ayah'),
+                    'no_paspor_ayah' => $this->request->getVar('no_paspor_ayah'),
+                    'tgl_paspor_ayah' => $this->request->getVar('tgl_paspor_ayah'),
+                    'rentang_paspor_ayah' => $this->request->getVar('rentang_paspor_ayah'),
+                    'nama_kakek' => $this->request->getVar('nama_kakek'),
+                    'pekerjaan' => $this->request->getVar('pekerjaan'),
+                    'status_sipil' => $this->request->getVar('status_sipil'),
+                    'negara' => $this->request->getVar('negara'),
+                    'tujuan' => $this->request->getVar('tujuan'),
+                ]);
+            };
+    
+            session()->setFlashdata('success', 'Data berhasil ditambahkan!');
+            return redirect()->to('/perdimanak');
+        }
     }
 }
--- app/Views/pemohon/perdimanak.php
+++ app/Views/pemohon/perdimanak.php
@@ -36,6 +36,16 @@
 					<div class="sb-msg"><i class="icon-check"></i><strong>Success!</strong> <?= session()->getFlashdata('success'); ?></div>
 				</div>
 			<?php endif; ?>
+
+			<?php if(session()->getFlashdata('error')) : ?>
+				<div class="style-msg errormsg">
+					<div class="sb-msg"><i class="icon-remove"></i>
+						<strong>Data anda tidak valid!</strong>
+						<?= session()->getFlashdata('error'); ?>
+					</div>
+				</div>
+			<?php endif; ?>
+
 			<form class="row" id="checkout-form" action="/createperdimanak" method="post" enctype="multipart/form-data">
 				<?= csrf_field(); ?>
 				<div class="form-process">
@@ -50,11 +60,11 @@
 						</div>
 						<div class="col-12 form-group">
 							<label>Nama Lengkap Anak:</label>
-							<input type="text" name="nama_anak" id="nama_anak" class="form-control required" value="" placeholder="" autofocus>
+							<input type="text" name="nama_anak" id="nama_anak" class="form-control <?= ($validation->hasError('nama_anak')) ? 'is-invalid' : '' ?>" value="" placeholder="">
 						</div>
 						<div class="col-12 form-group">
 							<label>Jenis Kelamin Anak:</label>
-							<select class="form-select required" name="jenis_kelamin" id="jenis_kelamin">
+							<select class="form-select <?= ($validation->hasError('jenis_kelamin')) ? 'is-invalid' : '' ?>" name="jenis_kelamin" id="jenis_kelamin">
 								<option>-Pilih Jenis Kelamin-</option>
 								<option value="Laki-Laki">Laki-Laki</option>
 								<option value="Perempuan">Perempuan</option>
@@ -62,7 +72,7 @@
 						</div>
 						<div class="col-12 form-group">
 							<label>Jenis Permohonan Paspor:</label>
-							<select class="form-select jenis_permohonan required" name="jenis_permohonan" id="jenis_permohonan">
+							<select class="form-select jenis_permohonan <?= ($validation->hasError('jenis_permohonan')) ? 'is-invalid' : '' ?>" name="jenis_permohonan" id="jenis_permohonan">
 								<option>-Pilih Jenis Permohonan Paspor Anda-</option>
 								<option value="Baru">Baru</option>
 								<option value="Penggantian">Penggantian</option>
@@ -86,15 +96,18 @@
 						</div>
 						<div class="col-12 form-group">
 							<label>Tempat/Tanggal Lahir Anak:</label><br>
-							<input type="text" name="ttl_anak" id="ttl_anak" class="form-control required" value="" placeholder="Kediri, 01-01-2010">
+							<input type="text" name="ttl_anak" id="ttl_anak" class="form-control <?= ($validation->hasError('ttl_anak')) ? 'is-invalid' : '' ?>" value="" placeholder="Kediri, 01-01-2010">
 						</div>
 						<div class="col-4 form-group">
 							<label>NIK / No. KIA:</label><br>
-							<input type="text" name="nik" id="nik" class="form-control required" value="" placeholder="">
+							<input type="text" name="nik" id="nik" class="form-control <?= ($validation->hasError('nik')) ? 'is-invalid' : '' ?>" value="" placeholder="">
+							<div class="invalid-feedback">
+								NIK harus terdiri dari 16 digit.
+							</div>
 						</div>
 						<div class="col-4 form-group">
 							<label>Tgl Diberikan:</label><br>
-							<input type="text" name="tgl_berlaku_kia" id="tgl_berlaku_kia" class="form-control required" value="" placeholder="01-12-2015">
+							<input type="text" name="tgl_berlaku_kia" id="tgl_berlaku_kia" class="form-control" value="" placeholder="01-12-2015">
 						</div>
 						<div class="col-4 form-group">
 							<label>berlaku s/d:</label><br>
@@ -102,7 +115,7 @@
 						</div>
 						<div class="col-6 form-group form-anak">
 							<label>Alamat Anak:</label>
-							<input type="text" name="alamat_anak" id="alamat_anak" class="form-control required" value="">
+							<input type="text" name="alamat_anak" id="alamat_anak" class="form-control <?= ($validation->hasError('alamat_anak')) ? 'is-invalid' : '' ?>" value="">
 							<!-- <label for="" id="anak-same-as-ortu" class="text-danger">
 								<input type="checkbox" class="me-2" value="1"> 
 								Alamat Anak Sama Dengan Alamat Orang Tua
@@ -125,24 +138,24 @@
 						</div>
 						<div class="col-12 form-group">
 							<label>No HP Ortu:</label>
-							<input type="text" name="no_hp" id="no_hp" class="form-control required" value="">
+							<input type="text" name="no_hp" id="no_hp" class="form-control <?= ($validation->hasError('no_hp')) ? 'is-invalid' : '' ?>" value="">
 						</div>
 						<div class="col-6 form-group">
 							<label>Nama Ibu:</label><br>
-							<input type="text" name="nama_ibu" id="nama_ibu" class="form-control required" value="" placeholder="">
+							<input type="text" name="nama_ibu" id="nama_ibu" class="form-control" value="" placeholder="">
 						</div>
 						<div class="col-6 form-group">
 							<label>Tempat/Tanggal Lahir Ibu:</label><br>
-							<input type="text" name="ttl_ibu" id="ttl_ibu" class="form-control required" value="" placeholder="Kediri, 03-03-1986">
+							<input type="text" name="ttl_ibu" id="ttl_ibu" class="form-control" value="" placeholder="Kediri, 03-03-1986">
 						</div>
 						
 						<div class="col-4 form-group">
 							<label>No. KTP Ibu:</label><br>
-							<input type="text" name="no_ktp_ibu" id="no_ktp_ibu" class="form-control required" value="" placeholder="">
+							<input type="text" name="no_ktp_ibu" id="no_ktp_ibu" class="form-control" value="" placeholder="">
 						</div>
 						<div class="col-4 form-group">
 							<label>Tgl Diberikan KTP Ibu:</label><br>
-							<input type="text" name="tgl_ktp_ibu" id="no_ktp_ibu" class="form-control required" value="" placeholder="">
+							<input type="text" name="tgl_ktp_ibu" id="no_ktp_ibu" class="form-control" value="" placeholder="">
 						</div>
 						<div class="col-4 form-group">
 							<label>Berlaku s/d:</label><br>
@@ -154,16 +167,16 @@
 						</div>
 						<div class="col-6 form-group">
 							<label>Nama Ayah:</label><br>
-							<input type="text" name="nama_ayah" id="nama_ayah" class="form-control required" value="" placeholder="">
+							<input type="text" name="nama_ayah" id="nama_ayah" class="form-control" value="" placeholder="">
 						</div>
 						<div class="col-6 form-group">
 							<label>Tempat/Tanggal Lahir Ayah:</label><br>
-							<input type="text" name="ttl_ayah" id="ttl_ayah" class="form-control required" value="" placeholder="Kediri, 02-02-1985">
+							<input type="text" name="ttl_ayah" id="ttl_ayah" class="form-control" value="" placeholder="Kediri, 02-02-1985">
 						</div>
 						
 						<div class="col-4 form-group">
 							<label>No. KTP Ayah:</label><br>
-							<input type="text" name="no_ktp_ayah" id="no_ktp_ayah" class="form-control required" value="" placeholder="">
+							<input type="text" name="no_ktp_ayah" id="no_ktp_ayah" class="form-control" value="" placeholder="">
 						</div>
 						<div class="col-4 form-group">
 							<label>Tgl Diberikan KTP Ayah:</label><br>
@@ -179,7 +192,7 @@
 						</div>
 						<div class="col-4 form-group">
 							<label>Tujuan Pengajuan Paspor:</label>
-							<select class="form-select tujuan required" name="tujuan" id="tujuan">
+							<select class="form-select tujuan <?= ($validation->hasError('tujuan')) ? 'is-invalid' : '' ?>" name="tujuan" id="tujuan">
 								<option>-Pilih Tujuan Anda-</option>
 								<option value="Wisata">Wisata</option>
 								<option value="Kunjungan">Kunjungan</option>
@@ -194,11 +207,11 @@
 						</div>
 						<div class="col-6 form-group">
 							<label>Negara Tujuan:</label><br>
-							<input type="text" name="negara" id="negara" class="form-control" value="" placeholder="">
+							<input type="text" name="negara" id="negara" class="form-control <?= ($validation->hasError('negara')) ? 'is-invalid' : '' ?>" value="" placeholder="">
 						</div>
 						<div class="col-6 form-group">
 							<label>Status Sipil:</label>
-							<select class="form-select tujuan required" name="status_sipil" id="status_sipil">
+							<select class="form-select tujuan <?= ($validation->hasError('status_sipil')) ? 'is-invalid' : '' ?>" name="status_sipil" id="status_sipil">
 								<option>-Pilih Status Sipil Anak-</option>
 								<option value="Kawin">Kawin</option>
 								<option value="Belum Kawin">Belum Kawin</option>
@@ -206,7 +219,7 @@
 						</div>
 						<div class="col-12 form-group">
 							<label>Pekerjaan:</label>
-							<input type="text" name="pekerjaan" id="pekerjaan" class="form-control required" value="">
+							<input type="text" name="pekerjaan" id="pekerjaan" class="form-control <?= ($validation->hasError('pekerjaan')) ? 'is-invalid' : '' ?>" value="">
 						</div>
 					</div>
 				</div>
