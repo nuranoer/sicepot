cb0fc602c7573362457425b8fa1eb6c3ee062779
tataadn <adine.pramesti@gmail.com>
2022-08-26 07:27:19 +0700

    fix sosmed

--- app/Views/pemohon/layout/template.php
+++ app/Views/pemohon/layout/template.php
@@ -170,7 +170,7 @@
 
 						<div class="col-md-6 text-center text-md-end">
 							<div class="d-flex justify-content-center justify-content-md-end">
-								<a href="#" class="social-icon si-small si-borderless si-facebook">
+								<!-- <a href="#" class="social-icon si-small si-borderless si-facebook">
 									<i class="icon-facebook"></i>
 									<i class="icon-facebook"></i>
 								</a>
@@ -178,36 +178,36 @@
 								<a href="#" class="social-icon si-small si-borderless si-twitter">
 									<i class="icon-twitter"></i>
 									<i class="icon-twitter"></i>
-								</a>
+								</a> -->
 
-								<a href="#" class="social-icon si-small si-borderless si-gplus">
+								<!-- <a href="#" class="social-icon si-small si-borderless si-gplus">
 									<i class="icon-gplus"></i>
 									<i class="icon-gplus"></i>
+								</a> -->
+
+								<a href="https://kediri.imigrasi.go.id/" class="social-icon si-small si-borderless si-google">
+									<i class="icon-google"></i>
+									<i class="icon-google"></i>
 								</a>
 
-								<a href="#" class="social-icon si-small si-borderless si-pinterest">
-									<i class="icon-pinterest"></i>
-									<i class="icon-pinterest"></i>
+								<a href="https://www.instagram.com/imigrasi_kediri" class="social-icon si-small si-borderless si-instagram">
+									<i class="icon-instagram"></i>
+									<i class="icon-instagram"></i>
 								</a>
 
-								<a href="#" class="social-icon si-small si-borderless si-vimeo">
-									<i class="icon-vimeo"></i>
-									<i class="icon-vimeo"></i>
+								<a href="https://www.tiktok.com/@imigrasi_kediri?_t=8V8pjPtRRgn&_r=1" class="social-icon si-small si-borderless si-tiktok">
+									<i class="icon-tiktok"></i>
+									<i class="icon-tiktok"></i>
 								</a>
 
-								<a href="#" class="social-icon si-small si-borderless si-github">
-									<i class="icon-github"></i>
-									<i class="icon-github"></i>
+								<a href="https://wa.me/6281133378284" class="social-icon si-small si-borderless si-whatsapp">
+									<i class="icon-whatsapp"></i>
+									<i class="icon-whatsapp"></i>
 								</a>
 
-								<a href="#" class="social-icon si-small si-borderless si-yahoo">
-									<i class="icon-yahoo"></i>
-									<i class="icon-yahoo"></i>
-								</a>
-
-								<a href="#" class="social-icon si-small si-borderless si-linkedin">
-									<i class="icon-linkedin"></i>
-									<i class="icon-linkedin"></i>
+								<a href="mailto:imigrasi.kediri@gmail.com" class="social-icon si-small si-borderless si-email3">
+									<i class="icon-gmail"></i>
+									<i class="icon-gmail"></i>
 								</a>
 							</div>
 
